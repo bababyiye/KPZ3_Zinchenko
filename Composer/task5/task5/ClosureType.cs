4e7effefdea5caa9d273e458b28291d4444f08bc
Maxim <ipz235_zmv@student.ztu.edu.ua>
2025-05-22 01:38:48 +0300

    add feature 2

--- Composer/task5/task5/ClosureType.cs
+++ Composer/task5/task5/ClosureType.cs
@@ -8,4 +8,3 @@ namespace task5
         OpenAndClose
     }
 }
-
---
+++ Composer/task5/task5/LightNodeEnumerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace task5
+{
+    public class LightNodeEnumerator : IEnumerable<LightNode>
+    {
+        private readonly LightNode _root;
+
+        public LightNodeEnumerator(LightNode root)
+        {
+            _root = root;
+        }
+
+        public IEnumerator<LightNode> GetEnumerator()
+        {
+            return Traverse(_root).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private IEnumerable<LightNode> Traverse(LightNode node)
+        {
+            yield return node;
+
+            if (node is LightElementNode element)
+            {
+                foreach (var child in element.Children)
+                {
+                    foreach (var descendant in Traverse(child))
+                        yield return descendant;
+                }
+            }
+        }
+    }
+}
