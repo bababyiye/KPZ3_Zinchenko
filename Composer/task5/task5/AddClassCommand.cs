d36f4fad5cac67aa3ed3876af33ce004cf673230
Maxim <ipz235_zmv@student.ztu.edu.ua>
2025-05-22 01:43:53 +0300

    add feature3

---
+++ Composer/task5/task5/AddClassCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace task5
+{
+    public class AddClassCommand : ICommand
+    {
+        private readonly LightElementNode _target;
+        private readonly string _cssClass;
+
+        public AddClassCommand(LightElementNode target, string cssClass)
+        {
+            _target = target;
+            _cssClass = cssClass;
+        }
+
+        public void Execute()
+        {
+            _target.AddCssClassInternal(_cssClass);
+        }
+    }
+}
